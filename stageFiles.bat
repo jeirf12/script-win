9d6714e765adf2040aeed3b1818cd818f6141467
jhonfer <jhonfer.ruiz@gmail.com>
2021-04-25 19:56:11 -0500

    add a new folder and the line end

--- stageFiles.bat
+++ stageFiles.bat
@@ -21,7 +21,7 @@ REM Carpeta a la cual se va mover los archivos
 set pathDate= E:\Music\Nueva\
 
 REM Crea la carpeta de nueva, sino existe (valida existencia de la carpeta)
-if not exist pathDate (mkdir %pathDate%)
+if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
 FOR %%f IN (*.mp3) DO (MOVE %pathCurrent%"%%f" %pathDate%)
@@ -30,7 +30,7 @@ REM Carpeta a la cual se va mover los archivos
 set pathDate= %pathCurrent%Pictures\
 
 REM Crea la carpeta de pictures, sino existe (valida existencia de la carpeta)
-if not exist pathDate (mkdir %pathDate%)
+if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
 FOR %%f IN (*.png *.jpg) DO (MOVE %pathCurrent%"%%f" %pathDate%)
@@ -39,7 +39,19 @@ REM Carpeta a la cual se va mover los archivos
 set pathDate= %pathCurrent%\Movies\
 
 REM Crea la carpeta de movies, sino existe (valida existencia de la carpeta)
-if not exist pathDate (mkdir %pathDate%)
+if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
 FOR %%f IN (*.mp4) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+
+REM Carpeta a la cual se va mover los archivos
+set pathDate= %pathCurrent%\Documents\
+
+REM Crea la carpeta de Documents, sino existe (valida existencia de la carpeta)
+if not exist %pathDate% (mkdir %pathDate%)
+
+REM Mueve los archivos a esa carpeta
+FOR %%f IN (*.doc *.pdf *.txt) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+
+REM Vuelve a la ruta inicial
+cd %pathPresent%
