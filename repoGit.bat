45cf7ea0a4b976d77a50adb86578bfca1f671899
jhonfer <jhonfer.ruiz@gmail.com>
2021-08-31 10:46:43 -0500

    news settings command repoGit to verify working tree

--- repoGit.bat
+++ repoGit.bat
@@ -1,11 +1,14 @@
 @echo off
 
+::================================Inicio========================================
 title repogit
   setlocal EnableExtensions EnableDelayedExpansion
       :Init
         set args=0
         for %%x in (%*) do set /A args+=1
         if %args% equ 1 (
+          if "%~1"=="" goto :ModeUse
+          if "%~1"=="''" goto :ModeUse
           call :VerifiedStatus %1
         ) else (
           call :ModeUse
@@ -14,51 +17,46 @@ title repogit
 
       :VerifiedStatus
         if exist .git/ (
-          git status >NUL 2>&1 | findstr /B "^Changes ^Untracked" >NUL
-          if %errorlevel% equ 0 (
-            call :Longitude %1
-          ) else (
-            echo/ [*] No hay cambios pendientes para subir
-          )
+          for /F "delims= " %%f in ('"(git status | findstr /R "^Changes ^Untracked") | find /c /v """') do set/a status=%%f
+          call :VerifiedStatus2 %1
         ) else (
           echo/ [*] Dirijase a un directorio valido
         )
         goto Finish
 
-      :ModeUse
-        echo Modo de uso.
-        echo ./repoGit "mensajeCommit"
+      :VerifiedStatus2
+        if "%status%" geq "1" (
+          call :Longitude %1
+        ) else (
+          echo/ [*] No hay cambios pendientes para subir
+        )
         goto Finish
+
+      :ModeUse
+        echo  Modo de uso
+        echo. .\repoGit "Mensaje del Commit"
+        goto Finish
+
+      :: Mide la longitud de una cadena
+      :Longitude
+	    :: Podemos usar también el metacaracter $
+	    FOR /F "DELIMS=:" %%F IN (
+		    '"(ECHO.%~1 & ECHO.) | FINDSTR /O ."'
+	    ) DO SET/A LEN=%%~F-3
+
+      :ContinueSequence
+        if %LEN% gtr 5 (
+          git branch | for /f "tokens=2" %%a in ('findstr "*"') do @echo %%a >temp.txt
+          set/p branch=< temp.txt
+          del temp.txt
+          git add .
+          git commit -m "%~1"
+          git push -u origin %branch%
+        ) else (
+          echo/ [*] Digite un mensaje mayor a 5 caracteres
+        )
+
+      :Finish
+        exit /B
   endlocal
-
-==============================================================
-
-:: Mide la longitud de una cadena
-:: Devuelve errorlevel=1 si no se especifica la cadena
-
-:Longitude
-  setlocal EnableExtensions
-	  :: Salimos si no se especifican argumentos
-	  IF "%~1"=="" exit /B
-	  :: Podemos usar también el metacaracter $
-	  FOR /F "DELIMS=:" %%F IN (
-		  '"(ECHO.%~1 & ECHO.) | FINDSTR /O ."'
-	  ) DO SET/A LEN=%%~F-3
-  endlocal & SET "LEN=%LEN%"
-
-:ContinueSequence
-  if %LEN% gtr 5 (
-    git branch | for /f "tokens=2" %%a in ('findstr "*"') do @echo %%a >temp.txt
-    set/p branch=< temp.txt
-    del temp.txt
-    git add .
-    git commit -m "%~1"
-    git push -u origin %branch%
-  ) else (
-    echo/ [*] Digite un mensaje mayor a 5 caracteres
-  )
-
-:Finish
-  exit /B
-
-
+::=================================Final========================================
