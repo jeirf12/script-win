4ea0a365b7485e4c38b891de7c7d922a704d59f1
jhonfer <jhonfer.ruiz@gmail.com>
2021-09-01 17:27:38 -0500

    update setter path

--- stageFiles.bat
+++ stageFiles.bat
@@ -2,14 +2,8 @@
 
 TITLE "Organize Information"
 
-REM Obtiene la ruta actual en un archivo plano
-cd >pathPresent.txt
-
-REM Lee el archivo y lo mete a la variable
-set/p pathPresent= <pathPresent.txt
-
-REM Elimina el archivo plano
-del pathPresent.txt
+REM Obtiene la ruta actual y la guarda en la pila de rutas
+pushd %cd%
 
 REM Carpeta a organizar
 set pathCurrent= %USERPROFILE%\Downloads\
@@ -24,7 +18,7 @@ REM Crea la carpeta de nueva, sino existe (valida existencia de la carpeta)
 if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
-FOR %%f IN (*.mp3) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+FOR %%f IN (*.mp3) DO (MOVE %pathCurrent%"%%f" %pathDate%) >NUL 2>&1
 
 REM Carpeta a la cual se va mover los archivos
 set pathDate= %pathCurrent%Pictures\
@@ -33,7 +27,7 @@ REM Crea la carpeta de pictures, sino existe (valida existencia de la carpeta)
 if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
-FOR %%f IN (*.png *.jpg) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+FOR %%f IN (*.png *.jpg) DO (MOVE %pathCurrent%"%%f" %pathDate%) >NUL 2>&1
 
 REM Carpeta a la cual se va mover los archivos
 set pathDate= %pathCurrent%\Movies\
@@ -42,7 +36,7 @@ REM Crea la carpeta de movies, sino existe (valida existencia de la carpeta)
 if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
-FOR %%f IN (*.mp4) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+FOR %%f IN (*.mp4) DO (MOVE %pathCurrent%"%%f" %pathDate%) >NUL 2>&1
 
 REM Carpeta a la cual se va mover los archivos
 set pathDate= %pathCurrent%\Documents\
@@ -51,7 +45,7 @@ REM Crea la carpeta de Documents, sino existe (valida existencia de la carpeta)
 if not exist %pathDate% (mkdir %pathDate%)
 
 REM Mueve los archivos a esa carpeta
-FOR %%f IN (*.doc *.pdf *.txt) DO (MOVE %pathCurrent%"%%f" %pathDate%)
+FOR %%f IN (*.doc *.pdf *.txt) DO (MOVE %pathCurrent%"%%f" %pathDate%) >NUL 2>&1
 
-REM Vuelve a la ruta inicial
-cd %pathPresent%
+REM Vuelve a la ruta inicial sacando de la pila de rutas
+popd
