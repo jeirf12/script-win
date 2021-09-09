3c98570361223cbfc68595c8097e8707420b31a5
jhonfer <jhonfer.ruiz@gmail.com>
2021-09-09 15:55:13 -0500

    update messages with colours

--- passwordWifi.bat
+++ passwordWifi.bat
@@ -1,5 +1,22 @@
 @echo off
 
+::================================Inicio========================================
+:: Todo este fragmento de código se necesita para los colores
+for /F %%a in ('echo prompt $E ^| cmd') do (
+    set "ESC=%%a"
+)
+:: Se puede definir mas codigos de colores aqui
+:: OJO: no borrar el endColour porque puede ocasionar 
+:: variaciones en la ejecucion de este codigo
+
+set redColour=%ESC%[31m
+set greenColour=%ESC%[32m
+set yellowColour=%ESC%[33m
+set blueColour=%ESC%[34m
+set purpleColour=%ESC%[35m
+set grayColour=%ESC%[37m
+set endColour=%ESC%[0m
+
 title Password Decode
   setlocal EnableExtensions EnableDelayedExpansion
     :Init
@@ -22,24 +39,24 @@ title Password Decode
       goto Finish
 
     :PasswordTodo
-      echo          SSID              Password
+      echo %greenColour%         SSID              Password%endColour%
       echo.
-      for /F "tokens=5,*" %%a in ('netsh wlan show profiles') do for /f "tokens=4,*" %%c in ('"(netsh wlan show profiles "%%a %%b" key=clear & netsh wlan show profiles "%%a%%b" key=clear) | findstr /R "*Key Content*""') do @echo %%a %%b                  %%c %%d     
+      for /F "tokens=5,*" %%a in ('netsh wlan show profiles') do for /f "tokens=4,*" %%c in ('"(netsh wlan show profiles "%%a %%b" key=clear & netsh wlan show profiles "%%a%%b" key=clear) | findstr /R "*Key Content*""') do @echo %blueColour%%%a %%b%endColour%                  %yellowColour%%%c %%d%endColour%
       goto Finish
 
     :PasswordSSID
-      echo          SSID              Password
-      for /f "tokens=4,*" %%c in ('"netsh wlan show profiles "%~2" key=clear | findstr /R "*Key Content*""') do @echo %~2             %%c %%d 
+      echo %greenColour%         SSID              Password%endColour%
+      for /f "tokens=4,*" %%c in ('"netsh wlan show profiles "%~2" key=clear | findstr /R "*Key Content*""') do @echo %blueColour%%~2%endColour%             %yellowColour%%%c %%d%endColour%
       goto Finish
     
     :ModeUse
-      echo Modo de uso
-      echo  .\passwordWifi
-      echo      -t         Muestra todas las redes con su ssid y su contrasenia
-      echo      -s "ssid"  Muestra la contrasenia de la red [ssid] pasada por argumentos
-      echo      -h         Muestra esta ayuda
+      echo %yellowColour%Modo de uso: .\passwordWifi%endColour%
+      echo      %purpleColour%-t%endColour%         %yellowColour%Muestra todas las redes con su ssid y su contrasenia%endColour%
+      echo      %purpleColour%-s%endColour%%redColour% "ssid"%endColour%  %yellowColour%Muestra la contrasenia de la red [ssid] pasada por argumentos%endColour%
+      echo      %purpleColour%-h%endColour%         %yellowColour%Muestra esta ayuda%endColour%
       goto Finish
 
     :Finish
       exit /B
   endlocal
+::=================================Final========================================
