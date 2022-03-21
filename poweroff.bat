34d4f287f221cc3f7f6d8ff78ccc36a55f95df7c
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2022-03-20 21:45:22 -0500

    news commands

---
+++ poweroff.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:: @brief Apaga el equipo
+:: @params /s indica apagar
+:: @params /t indica el tiempo de espera seguido de un numero (en este caso cero)
+shutdown /s /t 0
---
+++ reboot.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:: @brief Reinicia el equipo
+:: @params /r indica reiniciar
+:: @params /t indica el tiempo de espera seguido de un numero (en este caso cero)
+shutdown /r /t 0
