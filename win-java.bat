b8e2959ee010e37ac76f68d9fbd835cb66e6f74b
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2023-09-30 08:05:40 -0500

    changed version java

--- win-java.bat
+++ win-java.bat
@@ -7,11 +7,11 @@ IF ERRORLEVEL 1 CALL :DEFAULT_CASE
 exit /B 
 
 :CASE_8
-  set name=1.8.0_202
+  set name=8.0.382.5
   GOTO :SET_JAVA %name%
 
 :CASE_17
-  set name=-17.0.5
+  set name=17.0.8.101
   GOTO :SET_JAVA %name%
 
 :DEFAULT_CASE
@@ -28,7 +28,7 @@ exit /B
   GOTO :EOF
 
 :SET_JAVA %name%
-  set path_java=%PROGRAMFILES%\Java\jdk%name%
+  set path_java="%PROGRAMFILES%\Eclipse Adoptium\jdk-%name%-hotspot"
   setx JAVA_HOME "%path_java%"
   set JAVA_HOME=%path_java%
   set PATH=%JAVA_HOME%\bin;%PATH%
