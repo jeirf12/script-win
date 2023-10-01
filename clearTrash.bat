70fb3cfe238c11e19018da347609053e67fc89ec
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2023-10-01 09:10:33 -0500

    changed path disk

--- clearTrash.bat
+++ clearTrash.bat
@@ -14,8 +14,8 @@ rd /S /q %USERPROFILE%\AppData\Local\npm-cache\ >NUL 2>&1
 REM Vacia la papelera del disco C
 rd /s /q %SYSTEMDRIVE%\$Recycle.bin >NUL 2>&1
 
-REM Vacia la papelera del disco E
-rd /s /q E:\$Recycle.bin >NUL 2>&1
+REM Vacia la papelera del disco D
+rd /s /q D:\$Recycle.bin >NUL 2>&1
 
 REM Elimina archivos automatico e indica espacio libre y elimina archivos de sistemas anteriores
 cleanmgr /VERYLOWDISK /SETUP /AUTOCLEAN >NUL 2>&1
