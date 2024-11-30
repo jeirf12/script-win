0c3e3abcdd099b3d26d9b00f8963038a02a2aceb
Jhonfer Ruiz <jhonfer.ruiz@flyrlabs.com>
2024-11-30 12:37:46 -0500

    added commands

--- xj.bat
+++ xj.bat
@@ -1,4 +1,4 @@
 @echo off
 
 :: Generar json en un archivo en c#
-echo System.IO.File.WriteAllText(@"C:\\LogsFlyrs\\event.json", Newtonsoft.Json.JsonConvert.SerializeObject(event))| clip
+echo System.IO.File.WriteAllText(@"C:\LogsFlyrs\event.json", Newtonsoft.Json.JsonConvert.SerializeObject(event))| clip
--- xx.bat
+++ xx.bat
@@ -1,4 +1,4 @@
 @echo off
 
 :: Comando generar Xml en C#
-echo System.IO.File.WriteAllText(@"C:\\LogsFlyrs\\event.xml", new System.Xml.Serialization.XmlSerializer(typeof(event)).Serialize(new System.IO.StringWriter(), event).ToString())| clip
+echo "System.IO.File.WriteAllText(@"C:\LogsFlyrs\event.xml", new System.Func<string>(() => { var stringWriter = new System.IO.StringWriter(); new System.Xml.Serialization.XmlSerializer(typeof(EVENT)).Serialize(stringWriter, EVENT); return stringWriter.ToString(); })() );" | clip
